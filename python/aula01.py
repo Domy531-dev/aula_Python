8256cc347c76b2dffdfb7a4590fd9aad215d61ac
Vinicius Munuiz Silva Souza <Viniciussilva775.vs@gmail.com>
2025-04-08 17:47:13 -0300

    Add files via upload

---
+++ python/aula01.py
@@ -0,0 +1,39 @@
+'''
+Inteiros= INT
+Palavra/Letras=STRING
+Fracionado/Flutuante=FLOAT
+'''
+def bom_dia():
+    print("Hello Word!")
+    
+
+bom_dia()
+
+def saudacao(nome):
+    print(f"olá, {nome}! Seja bem-vindo ao mundo do Python")
+
+a = "Vinícius" 
+saudacao(a)
+
+
+def soma():
+    resultado = 2+2
+    print(f"O resultado é: {resultado}")
+
+def subtracao():
+    resultado = a - b
+    print(f"O resultado é: {resultado}")
+
+
+def multiplicacao():
+    resultado = a * b 
+    print(f"O resultado é: {resultado}")
+
+
+def divisao():
+    resultado = a / b  
+    print(f"O resutado é: {resultado}")
+
+    divisao(50, 7)
+      
+    multiplicacao(9, 7)     
